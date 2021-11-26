637fed3f0836da6c4260d242b145d8e00782eeb2
booldook <booldook@gmail.com>
2021-11-26 10:54:44 +0900

    navi-slice

--- src/store/reducers/navi-slice.js
+++ src/store/reducers/navi-slice.js
@@ -1,29 +1,41 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { 액션Async } from '../apis/액션-api';
+import axios from 'axios';
 
 /** state *************/
-const state = {};
+const state = {
+  selTree: '',
+  allTree: [],
+};
 
 /** async action ******/
-export const 액션명 = createAsyncThunk('네임/액션명', async (매개변수) => {
-  const 데이터 = await 액션Async(매개변수);
-  return 데이터;
+export const asyncTree = createAsyncThunk('tree/asyncTree', async () => {
+  const url = 'http://127.0.0.1:3100/api/tree';
+  const { data } = await axios.get(url);
+  const tree = data[0].children.map((v) => {
+    let children = v.children.map((v2) => ({ id: v2.id, title: v2.text }));
+    return { id: v.id, title: v.text, children };
+  });
+  return tree;
 });
 
 /** reducer ***********/
-export const 슬라이스네임 = createSlice({
-  name: '네임',
+export const treeSlice = createSlice({
+  name: 'tree',
   state,
   reducers: {
-    액션크리에이터명: () => {},
+    selTree: (state, payload) => {
+      state.selTree = payload;
+    },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(액션명.pending, (state, action) => {})
-      .addCase(액션명.fulfilled, (state, action) => {})
-      .addCase(액션명.rejected, (state, action) => {});
+      .addCase(asyncTree.pending, (state, action) => {})
+      .addCase(asyncTree.fulfilled, (state, action) => {})
+      .addCase(asyncTree.rejected, (state, action) => {});
   },
 });
 
 /** method ************/
+export const 사용자함수 = (state) => state.data;
+export const { 동기액션 } = 슬라이스네임.actions;
 export default 슬라이스네임.reducer;
