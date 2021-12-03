5d7fd53aeccdedb4c14e2d1e47e1b52d2247baf1
booldook <booldook@gmail.com>
2021-12-03 15:08:21 +0900

    PrdWrapperCp.js

--- src/components/prd/PrdWrapperCp.js
+++ src/components/prd/PrdWrapperCp.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '../../style';
+
+import { prdApi } from '../../modules/api';
 
 const Wrapper = styled.ul``;
 
 const PrdWrapperCp = () => {
+  const [prd, setPrd] = useState([]);
+  useEffect(() => {
+    (async () => setPrd(await prdApi()))();
+  }, []);
   return <Wrapper></Wrapper>;
 };
 
