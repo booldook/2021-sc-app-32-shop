6b1dc8377188639927e0b12c955cbf7fc0469391
booldook <booldook@gmail.com>
2021-12-09 13:22:46 +0900

    PrdWrapper

--- src/components/prd/PrdWrapperCp.js
+++ src/components/prd/PrdWrapperCp.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import styled, { media } from '../../style';
+import React, { useEffect, useState, useCallback } from 'react';
+import styled, { media, font, color } from '../../style';
 
 import { prdApi } from '../../modules/api';
 import PrdCp from './PrdCp';
+import ButtonCp from '../common/ButtonCp';
 
-const Wrapper = styled.ul`
+const PrdWrapper = styled.ul`
   margin-top: 1em;
   display: flex;
   flex-wrap: wrap;
@@ -21,19 +22,54 @@ const Wrapper = styled.ul`
   }
 `;
 
+const Title = styled.h2`
+  font-size: 1.5em;
+  font-weight: 500;
+  font-family: ${font.en};
+  margin: 2em 0 1em 0;
+  text-align: center;
+`;
+
+const Button = styled(ButtonCp)`
+  display: block;
+  width: 200px;
+  margin: auto;
+  padding: 1em 0;
+`;
+
 const PrdWrapperCp = () => {
   const [prd, setPrd] = useState([]);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     (async () => {
-      setPrd(await prdApi());
+      setPrd(await prdApi({ page }));
     })();
-  }, []);
+  }, [page]);
+
+  const onClick = useCallback(async () => {
+    setPage(page + 1);
+    let prds = [...prd, ...(await prdApi({ page }))];
+    console.log(prds);
+    setPrd(prds);
+  }, [page, prd]);
+
   return (
-    <Wrapper>
-      {prd.map((v, i) => (
-        <PrdCp {...v} key={i} />
-      ))}
-    </Wrapper>
+    <div>
+      <Title>New Products</Title>
+      <PrdWrapper>
+        {prd.map((v, i) => (
+          <PrdCp {...v} key={i + v.title} />
+        ))}
+      </PrdWrapper>
+      <Button
+        txt="SHOW MORE"
+        colorHover={color.light}
+        bgHover={color.dark}
+        bold="bold"
+        onClick={onClick}
+      />
+    </div>
   );
 };
 
