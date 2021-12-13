39747fb0bb7f20fe7ea00a733365d9f68a1ebfe3
booldook <booldook@gmail.com>
2021-12-13 12:22:10 +0900

    EmailCp

--- src/components/footer/EmailCp.js
+++ src/components/footer/EmailCp.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import styled, { color } from '../../style';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { makeStyles } from '@mui/styles';
+import { emailApi } from '../../modules/api';
 
 const Wrapper = styled.div`
   display: flex;
+  margin: 1.5em 0;
 `;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiFilledInput-root': {
-      background: 'rgb(255, 255, 255)',
+      background: '#fff',
     },
+    marginRight: '0.5em',
   },
 }));
 
 const EmailCp = () => {
   const classes = useStyles();
+  const emailRef = useRef(null);
+  const onSubmit = useCallback(async () => {
+    const email = emailRef.current.value.trim();
+    if (email === '') {
+      alert('Enter Your Email!');
+      emailRef.current.focus();
+      return false;
+    }
+    const result = await emailApi(email);
+    console.log(result);
+  }, [emailRef]);
   return (
     <Wrapper>
       <TextField
@@ -26,8 +40,12 @@ const EmailCp = () => {
         id="outlined-required"
         label="enter your email address"
         variant="filled"
+        color="success"
+        inputRef={emailRef}
       />
-      <Button />
+      <Button variant="contained" color="warning" onClick={onSubmit}>
+        SUBMIT
+      </Button>
     </Wrapper>
   );
 };
